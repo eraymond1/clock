9ef246605b6d17cb8f6a7dbc4ea6fc94be4dff33
eraymond1 <ericraymond@rocketmail.com>
2023-02-23 16:15:27 -0500

    added analog clock to page, updated script, css, and html

--- script.js
+++ script.js
@@ -36,4 +36,26 @@ updateClock();
 
 var oneSecond = 1000;
 
-setInterval( updateClock, oneSecond);
+setInterval(updateClock, oneSecond);
+
+setInterval(() => {    
+
+    d = new Date();
+
+    hr = d.getHours();
+    min = d.getMinutes()
+    sec = d.getSeconds();
+
+    hr_rotation = 30 * hr + min / 2;
+    min_rotation = 6 * min;
+    sec_rotation = 6 * sec;
+
+    hour.style.transform = `rotate(${hr_rotation}deg)`;
+    minute.style.transform = `rotate(${min_rotation}deg)`;
+    second.style.transform = `rotate(${sec_rotation}deg)`;
+
+}, 1000);
+
+   
+
+
